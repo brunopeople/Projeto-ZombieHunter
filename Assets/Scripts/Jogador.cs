e1f4320a5135816ed2d423905ef3c79e34d7e4e1
brunopeople <brunopessoa.melo@gmail.com>
2019-01-14 01:27:49 -0300

    implementando personagem e animação, erro de animação personagem não está ocorrendo

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour {
+    public float velocidadejogador = 10;
+	
+	void Update () {
+       float eixoX = Input.GetAxis("Horizontal");
+       float eixoZ = Input.GetAxis("Vertical");
+
+        Vector3 direcao = new Vector3(eixoX, eixoZ, 0);
+
+        transform.Translate(direcao * 10 * Time.deltaTime);
+	}
+}
