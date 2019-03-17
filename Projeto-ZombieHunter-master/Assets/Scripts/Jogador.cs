b1222efcdf9bcf9ac77f190a236dfeffd2b23c16
brunopeople <brunopessoa.melo@gmail.com>
2019-03-16 23:59:56 -0300

    Primeira animação ok

---
+++ Projeto-ZombieHunter-master/Assets/Scripts/Jogador.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour {
+    public float velocidadejogador = 10;
+	
+	void Update () {
+       float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
+        transform.Translate(direcao);
+   
+	}
+}
