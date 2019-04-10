8a34c4606e3a25baf8c6ab030be1b8df7cbfb6cc
brunopeople <brunopessoa.melo@gmail.com>
2019-04-10 01:03:55 -0300

    velocidade do personagem estabilizada, camera seguindo jogador

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public GameObject Jogador;
+    Vector3 calcula_distancia;
+	// Use this for initialization
+	void Start () {
+        calcula_distancia = transform.position - Jogador.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        calcula_distancia = transform.position + Jogador.transform.position;
+    }
+}
--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Jogador : MonoBehaviour {
-    public float velocidadejogador = 10;
+    public float velocidadejogador = 50;
 	
 	void Update () {
        float eixoX = Input.GetAxis("Horizontal");
         float eixoZ = Input.GetAxis("Vertical");
 
         Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
-        transform.Translate(direcao*velocidadejogador*Time.deltaTime);
+        transform.Translate(direcao * velocidadejogador * Time.deltaTime);
    
 	}
 }
